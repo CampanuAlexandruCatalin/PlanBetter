dd30918a5e033bb387c6f0c1e42cd9ae1525b101
emiliachelariu <emilia-mihaela.chelariu@student.tuiasi.ro>
2022-07-10 19:21:45 +0300

    #exam

---
+++ MyHotel.Domain/Entities/Exam.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanBetter.Domain.Entities
+{
+     public class Exam : BaseEntity
+    {
+        public int course_id { get; set; }
+        public int teacher_id { get; set; }
+        public int group_id { get; set; }
+        public DateTime date { get; set; }
+        public DateTime time_start { get; set; }
+        public DateTime time_end { get; set; }
+        public string room_no { get; set; }
+        public string details { get; set; }
+    }
+}
