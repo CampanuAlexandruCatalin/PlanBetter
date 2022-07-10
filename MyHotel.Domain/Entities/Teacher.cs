16e3d4072303bfdf9cd504ed09095a64676cd392
emiliachelariu <emilia-mihaela.chelariu@student.tuiasi.ro>
2022-07-10 18:52:07 +0300

    #teacher

---
+++ MyHotel.Domain/Entities/Teacher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanBetter.Domain.Entities
+{
+    public class Teacher:BaseEntity
+    {
+        public string email { get; set; }
+        public string password { get; set; }
+        public string fname { get; set; }
+        public string lname { get; set; }
+        public DateTime dob { get; set; }
+        public string mobile { get; set; }
+        public DateTime date_of_join { get; set; }
+        public bool status { get; set; }
+        public string job_title { get; set; }
+    }
+}
