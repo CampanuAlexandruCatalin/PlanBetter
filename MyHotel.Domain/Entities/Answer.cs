9dcdf97738b21961727abd669dcde94f40a73195
emiliachelariu <emilia-mihaela.chelariu@student.tuiasi.ro>
2022-07-10 19:30:39 +0300

    #answer

---
+++ MyHotel.Domain/Entities/Answer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanBetter.Domain.Entities
+{
+   public class Answer :BaseEntity
+    {
+        public int question_id { get; set; }
+        public string answer_text { get; set; }
+    }
+}
