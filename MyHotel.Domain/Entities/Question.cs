a1207ebee8608ce43c19a109787547ab944f3e01
emiliachelariu <emilia-mihaela.chelariu@student.tuiasi.ro>
2022-07-10 19:27:28 +0300

    #guestions

---
+++ MyHotel.Domain/Entities/Question.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanBetter.Domain.Entities
+{
+   public  class Question : BaseEntity
+    {
+        public int course_id { get; set; }
+        public string question_text { get; set; }
+    }
+}
