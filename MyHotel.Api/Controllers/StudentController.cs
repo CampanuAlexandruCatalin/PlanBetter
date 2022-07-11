bb6077a0bfe34f83db8c20e8e4adaba500d03b16
Campanu Alexandru Catalin <alexandru-catalin.campanu@student.tuiasi.ro>
2022-07-11 15:54:25 +0300

    #studentcontroller

---
+++ MyHotel.Api/Controllers/StudentController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using PlanBetter.Business.Services.IServices;
+
+namespace PlanBetter.Api.Controllers
+{
+    [Microsoft.AspNetCore.Components.Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+            private readonly IStudentService _studentService;
+
+            public StudentController(IStudentService studentService)
+            {
+                _studentService = studentService;
+            }
+
+            //[HttpGet]
+            //public IActionResult GetAll()
+            //{
+            //    return Ok(_studentService.GetStudents());
+            //}
+
+            [HttpGet("{id}")]
+            public IActionResult GetById(int id)
+            {
+                var room = _studentService.GetStudent(id);
+                if (room != null)
+                {
+                    return Ok(room);
+                }
+
+                return NotFound();
+
+            }
+
+
+            [HttpDelete("{id}")]
+            public IActionResult Delete(int id)
+            {
+                _studentService.DeleteStudent(id);
+                return NoContent();
+            }
+        }
+    }
+
---
+++ MyHotel.Business/Services/StudentService.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using PlanBetter.Business.Models;
+using PlanBetter.Business.Services.IServices;
+using PlanBetter.Domain.Entities;
+using PlanBetter.Domain.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanBetter.Business.Services
+{
+    internal class StudentService : IStudentService
+    {
+        private readonly IStudentRepository _studentRepository;
+        private readonly IMapper _mapper;
+
+        public StudentService(IStudentRepository studentRepository, IMapper mapper)
+        {
+            _studentRepository = studentRepository;
+            _mapper = mapper;
+        }
+        public int AddStudent(StudentModel student)
+        {
+            var newstudent = _studentRepository.Add(_mapper.Map<Student>(student));
+            return newstudent.Id;
+        }
+
+        public void DeleteStudent(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Student GetStudent(int id)
+        {
+            return _studentRepository.GetById(id);
+        }
+
+        public IEnumerable<Student> GetStudents()
+        {
+            return _studentRepository.ListAll();
+        }
+
+        public void UpdateStudent(Student student)
+        {
+            _studentRepository.Update(student);
+        }
+    }
+}
