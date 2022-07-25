f7cbbe16270cff74eaf8c3e675ed733bdc089b0c
Campanu Alexandru Catalin <alexandru-catalin.campanu@student.tuiasi.ro>
2022-07-25 16:15:22 +0300

    #25

--- MyHotel/Data/PlanBetterDbContext.cs
+++ MyHotel/Data/PlanBetterDbContext.cs
@@ -8,7 +8,6 @@ namespace PlanBetter.Persistance.Data
     public class PlanBetterDbContext : DbContext
     {
         
-
         public DbSet<Answer> Answers { get; set; }
         public DbSet<Course> Courses { get; set; } 
         public DbSet<Exam> Exams { get; set; } 
@@ -17,8 +16,6 @@ namespace PlanBetter.Persistance.Data
         public DbSet<Student> Students { get; set; }
         public DbSet<Teacher> Teachers { get; set; }
        
-        
-
         public PlanBetterDbContext(DbContextOptions<PlanBetterDbContext> options)
               : base(options)
         {
@@ -26,8 +23,6 @@ namespace PlanBetter.Persistance.Data
         
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-       
-            
             StudentMapping.Map(modelBuilder);
             ExamMapping.Map(modelBuilder);
             SeedDatabase(modelBuilder);
@@ -75,37 +70,145 @@ namespace PlanBetter.Persistance.Data
                     Status=false
                 }
             });
-            /*
+            modelBuilder.Entity<Teacher>().HasData(new List<Teacher>()
+            {
+                new Teacher()
+                {
+                    Id = 91,
+                    Email = "email1@facultate.profesor.com",
+                    Password = "parola123",
+                    FName = "profesor",
+                    LName = "unu",
+                    Dob = System.DateTime.Now,
+                    Mobile="1234",
+                    DateOfJoin=System.DateTime.Now,
+                    Status=false,
+                    JobTitle="laborant"
+                },
+                new Teacher()
+                {
+                    Id = 92,
+                    Email = "email2@facultate.profesor.com",
+                    Password = "admin123",
+                    FName = "profesor",
+                    LName = "doi",
+                    Dob = System.DateTime.Now,
+                    Mobile="07unudoi",
+                    DateOfJoin=System.DateTime.Now,
+                    Status=true,
+                    JobTitle="doc. ing."
+                },
+                new Teacher()
+                {
+                    Id = 93,
+                    Email = "email3@facultate.profesor.com",
+                    Password = "parola",
+                    FName = "profesor",
+                    LName = "trei",
+                    Dob = System.DateTime.Now,
+                    Mobile="0777666777",
+                    DateOfJoin=System.DateTime.Now,
+                    Status=false,
+                    JobTitle="profesor laborant"
+                }
+            });
+
+            modelBuilder.Entity<StudentGroup>().HasData(new List<StudentGroup>()
+            {
+                new StudentGroup()
+                {
+                    Id=1301,
+                    GroupName="c",
+                    StudentId=1
+                },
+                new StudentGroup()
+                {
+                    Id=1302,
+                    GroupName="ti",
+                    StudentId=2
+                }
+            });
+
             modelBuilder.Entity<Exam>().HasData(new List<Exam>()
             {
                 new Exam()
                 {
-                    Id = 1,
-                 CourseId = 100,
-                    TeacherId = 13,
-                    GroupId = 33,
+                    Id = 551,
+                    CourseId = 100,
+                    TeacherId = 91,
+                    GroupId = 1301,
                     Date = System.DateTime.Now,
                     TimeStart = System.DateTime.Now,
                     TimeEnd = System.DateTime.Now,
-                    RoomNo = "parter",
+                    RoomNo = "teams meet",
                     Details="examen grila"
 
                 },
                 new Exam()
                 {
-                    Id = 2,
-                   CourseId = 200,
-                    TeacherId = 3,
-                    GroupId = 43,
+                    Id = 552,
+                    CourseId = 200,
+                    TeacherId = 92,
+                    GroupId = 1302,
                     Date = System.DateTime.Now,
                     TimeStart = System.DateTime.Now,
                     TimeEnd = System.DateTime.Now,
-                    RoomNo = "etajul2",
+                    RoomNo = "google meet",
                     Details="examen scris"
                 }
             });
-            
-            */
+
+            modelBuilder.Entity<Course>().HasData(new List<Course>()
+            {
+                new Course()
+                {
+                   Id=200,
+                   CourseName="materie1",
+                   TeacherId=91,
+                   GroupId=1301
+
+                },
+                new Course()
+                {
+                    Id=100,
+                   CourseName="materie2",
+                   TeacherId=92,
+                   GroupId=1302
+                }
+            }) ;
+
+            modelBuilder.Entity<Question>().HasData(new List<Question>()
+            {
+                new Question()
+                {
+                  Id=991,
+                  CourseId=200,
+                  QuestionText="salut?",
+                  
+                },
+                new Question()
+                {
+                   Id=992,
+                  CourseId=100,
+                  QuestionText="?salut?",
+                }
+            });
+            modelBuilder.Entity<Answer>().HasData(new List<Answer>()
+            {
+                new Answer()
+                {
+                    Id=9991,
+                    QuestionId=991,
+                   AnswerText="da"
+
+                },
+                new Answer()
+                {
+                   Id=9992,
+                   QuestionId=992,
+                   AnswerText="da?"
+                }
+            });
         }
     }
 }
