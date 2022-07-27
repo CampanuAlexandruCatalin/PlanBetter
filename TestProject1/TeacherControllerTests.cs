f6de7826f45c520ffd1fa13fd410e1e0a334b456
emiliachelariu <100294712+emiliachelariu@users.noreply.github.com>
2022-07-27 17:52:28 +0300

    #teachercontrollertest

--- TestProject1/TeacherControllerTests.cs
+++ TestProject1/TeacherControllerTests.cs
@@ -59,62 +59,62 @@ namespace PalanBetter.IntegrationTests
             teacherFromResult.Id.ToString().Should().Be(teacherIdFromResult);
         }
 
-        //[TestMethod]
-        //public async Task When_UpdateDatabase_ShouldChangeTeacherData()
-        //{
-        //    //Arrange
-        //    var application = new WebApplicationFactory<Startup>()
-        //.WithWebHostBuilder(builder =>
-        //{
-        //    // ... Configure test services
-        //});
+        [TestMethod]
+        public async Task When_UpdateDatabase_ShouldChangeTeacherData()
+        {
+            //Arrange
+            var application = new WebApplicationFactory<Startup>()
+        .WithWebHostBuilder(builder =>
+        {
+            // ... Configure test services
+        });
 
-        //    var client = application.CreateClient();
-        //    var teacherModel = new TeacherModel()
-        //    {
-        //        Email = "profesor1@academic.com",
-        //        Password = "password",
-        //        FName = "fname",
-        //        LName = "lname",
-        //        Dob = new DateTime(2022, 7, 17, 16, 7, 35, 571, DateTimeKind.Local).AddTicks(8239),
-        //        Mobile = "0712",
-        //        Status = true,
-        //        DateOfJoin = new DateTime(2022, 7, 17, 16, 7, 35, 571, DateTimeKind.Local).AddTicks(8239),
-        //        JobTitle = "inginer",
-        //    };
-        //    var result = await client.PostAsJsonAsync("api/teacher", teacherModel);
-        //    result.EnsureSuccessStatusCode();
-        //    var teacherIdFromResult = await result.Content.ReadAsStringAsync();
+            var client = application.CreateClient();
+            var addTeacherModel = new AddTeacherModel()
+            {
+                Email = "profesor1@academic.com",
+                Password = "password",
+                FName = "fname",
+                LName = "lname",
+                Dob = new DateTime(2022, 7, 17, 16, 7, 35, 571, DateTimeKind.Local).AddTicks(8239),
+                Mobile = "0712",
+                Status = true,
+                DateOfJoin = new DateTime(2022, 7, 17, 16, 7, 35, 571, DateTimeKind.Local).AddTicks(8239),
+                JobTitle = "inginer",
+            };
+            var result = await client.PostAsJsonAsync("api/teacher", addTeacherModel);
+            result.EnsureSuccessStatusCode();
+            var teacherIdFromResult = await result.Content.ReadAsStringAsync();
 
-        //    var expectedChangeTeacherStatus = false;
-        //    var teacher = new Teacher()
-        //    {
-        //        Id = Convert.ToInt32(teacherIdFromResult),
-        //        Email = "profesor1@academic.com",
-        //        Password = "password",
-        //        FName = "fname",
-        //        LName = "lname",
-        //        Dob = new DateTime(2022, 7, 17, 16, 7, 35, 571, DateTimeKind.Local).AddTicks(8239),
-        //        Mobile = "0712",
-        //        DateOfJoin = new DateTime(2022, 7, 17, 16, 7, 35, 571, DateTimeKind.Local).AddTicks(8239),
-        //        JobTitle = "inginer",
-        //        Status = expectedChangeTeacherStatus
-        //    };
-        //    //Act
-        //    var resultForUpdateTeacher = await client.PutAsJsonAsync("api/teacher", teacher);
+            var expectedChangeTeacherStatus = false;
+            var teacher = new Teacher()
+            {
+                Id = Convert.ToInt32(teacherIdFromResult),
+                Email = "profesor1@academic.com",
+                Password = "password",
+                FName = "fname",
+                LName = "lname",
+                Dob = new DateTime(2022, 7, 17, 16, 7, 35, 571, DateTimeKind.Local).AddTicks(8239),
+                Mobile = "0712",
+                DateOfJoin = new DateTime(2022, 7, 17, 16, 7, 35, 571, DateTimeKind.Local).AddTicks(8239),
+                JobTitle = "inginer",
+                Status = expectedChangeTeacherStatus
+            };
+            //Act
+            var resultForUpdateTeacher = await client.PutAsJsonAsync("api/teacher", teacher);
 
 
-        //    //Assert
-        //    resultForUpdateTeacher.EnsureSuccessStatusCode();
+            //Assert
+            resultForUpdateTeacher.EnsureSuccessStatusCode();
 
-        //    var resultOfGetTeacherById = await client.GetAsync($"api/teahcer/{teacherIdFromResult}");
-        //    resultOfGetTeacherById.EnsureSuccessStatusCode();
-        //    var teacherFromResult = await resultOfGetTeacherById.Content.ReadFromJsonAsync<Teacher>();
+            var resultOfGetTeacherById = await client.GetAsync($"api/teacher/{teacherIdFromResult}");
+            resultOfGetTeacherById.EnsureSuccessStatusCode();
+            var teacherFromResult = await resultOfGetTeacherById.Content.ReadFromJsonAsync<Teacher>();
 
-        //    teacherFromResult.Should().NotBeNull();
-        //    teacherFromResult.Id.ToString().Should().Be(teacherIdFromResult);
+            teacherFromResult.Should().NotBeNull();
+            teacherFromResult.Id.ToString().Should().Be(teacherIdFromResult);
 
-        //    teacherFromResult.Status.Should().Be(expectedChangeTeacherStatus);
-        //}
+            teacherFromResult.Status.Should().Be(expectedChangeTeacherStatus);
+        }
     }
 }
